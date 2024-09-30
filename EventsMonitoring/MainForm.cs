2211a5c3f016bfd71e07d8abb6521ea22d3a0af2
kran313 <artem93.irk@mail.ru>
2024-09-30 11:08:40 +0300

    fixed hidden matches

--- EventsMonitoring/MainForm.cs
+++ EventsMonitoring/MainForm.cs
@@ -380,11 +380,11 @@ namespace EventsMonitoring
             {
                 isLive = true; ;
                 timeIntervalGroupBox.Visible = false;
-                button1.Visible = false;
-                button2.Visible = false;
-                button3.Visible = false;
-                button4.Visible = false;
-                dataGridView2.Visible = false;
+                button1.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                dataGridView2.Visible = true;
             }
 
             isRefreshable = true;
@@ -636,15 +636,9 @@ namespace EventsMonitoring
             var tempHiddenMatches = new List<Hidden>();
             foreach (var item in hiddenMatches.Values)
             {
-                if (item.startTime > DateTime.Now)
-                {
-                    tempHiddenMatches.Add(new Hidden(item.matchID, item.sport, item.branch, $"{item.team1.teamName} - {item.team2.teamName}"));
-                }
-                else
-                {
-                    hiddenMatches.Remove(item.matchID);
-                }
+                tempHiddenMatches.Add(new Hidden(item.matchID, item.sport, item.branch, $"{item.team1.teamName} - {item.team2.teamName}"));
             }
+
             dataGridView2.Columns[1].Visible = false;
             dataGridView2.Columns[2].Visible = true;
             dataGridView2.Columns[3].Visible = true;
