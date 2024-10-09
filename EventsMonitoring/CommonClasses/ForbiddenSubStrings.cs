6e09858a1578f22aaea2abe5ee3c1788c2d49691
kran313 <artem93.irk@mail.ru>
2024-10-09 12:35:01 +0300

    changebd again

--- EventsMonitoring/CommonClasses/ForbiddenSubStrings.cs
+++ EventsMonitoring/CommonClasses/ForbiddenSubStrings.cs
@@ -9,7 +9,7 @@ namespace EventsMonitoring.CommonClasses
 {
     public static class ForbiddenSubStrings
     {
-        private static string _connectionString = "data source=srvapkbb9.gkbaltbet.local;initial catalog=EventMatching;user Id=event_match;password=umbMzLQB3hKo69VG8F4N;" +
+        private static string _connectionString = "data source=analytic.gkbaltbet.local;initial catalog=EventMatching;user Id=event_match;password=umbMzLQB3hKo69VG8F4N;" +
                                   "Integrated Security=false;MultipleActiveResultSets=true;TrustServerCertificate=true";
         private static string _sqlExpression;
 
